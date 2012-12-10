5df7450143de3b6cbab877a226a9dbb08aeea892
Ryan Murphy <ryan.rdmurphy@gmail.com>
2012-12-10 14:07:19 -0600

    Added jQuery callback support

--- grabber.py
+++ grabber.py
@@ -1,15 +1,15 @@
 import os
 import requests
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
-RESERVOIR_JSON = "http://waterdatafortexas.org/reservoirs/recent-conditions.json"
+RESERVOIR_JSON = 'http://waterdatafortexas.org/reservoirs/recent-conditions.json'
 
 
 @app.route('/reservoirs.json')
 def get_reservoir_json():
     r = requests.get(RESERVOIR_JSON)
-    return "callback(" + r.text + ")"
+    return '{0}({1})'.format(request.args.get('callback'), r.text)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
